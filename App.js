2617835e66b0b6a73a074a61a7dc6240fcd65f7f
Kashish Kochar <kkochar8@gmail.com>
2024-06-30 22:48:48 +0530

    First push

---
+++ App.js
@@ -0,0 +1,7 @@
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const heading = React.createElement('h1', { id: 'heading', xyz: 'abc'}, 'Hello  World from React!');
+const heading2 = React.createElement('h2', {}, 'h2 tag');
+const child = React.createElement('div', { id: 'child' }, [heading, heading2]);
+const child2 = React.createElement('div', { id: 'child2' }, [heading, heading2]);
+const parent = React.createElement('div', { id: 'parent' }, [child, child2]);
+root.render(parent); 
